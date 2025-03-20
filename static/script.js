a715a650b3bc862692364596d23b5af80c807a5f
looo <linus.denzler@gmail.com>
2025-03-20 15:57:29 +0100

    login,SuS site and Tenses site added.

---
+++ static/script.js
@@ -0,0 +1,24 @@
+function toSuS() {
+    document.getElementById('login-container').classList.add('hidden');
+    document.getElementById('main-view').classList.remove('hidden');
+    document.getElementById('tense-selection-container').classList.add('hidden');
+
+}
+
+function toTenses() {
+    document.getElementById('login-container').classList.add('hidden');
+    document.getElementById('main-view').classList.add('hidden');
+    document.getElementById('tense-selection-container').classList.remove('hidden');
+}
+
+
+
+document.getElementById("login-button").addEventListener("click",toSuS);
+document.getElementById("home-button").addEventListener("click",toSuS);
+const destinations = document.getElementsByClassName("destination");
+
+// Loop through each element and add an event listener
+for (let i = 0; i < destinations.length; i++) {
+    destinations[i].addEventListener("click", toTenses);
+}
+
