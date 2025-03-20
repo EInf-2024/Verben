fe368d3995f5b042b408abefb2e8d38bc7cce561
looo <linus.denzler@gmail.com>
2025-03-20 16:09:46 +0100

    SuS seite rohes html und CSS fertig

--- static/script.js
+++ static/script.js
@@ -2,19 +2,29 @@ function toSuS() {
     document.getElementById('login-container').classList.add('hidden');
     document.getElementById('main-view').classList.remove('hidden');
     document.getElementById('tense-selection-container').classList.add('hidden');
-
+    document.getElementById('gap-text-container').classList.add('hidden');
 }
 
 function toTenses() {
     document.getElementById('login-container').classList.add('hidden');
     document.getElementById('main-view').classList.add('hidden');
     document.getElementById('tense-selection-container').classList.remove('hidden');
+    document.getElementById('gap-text-container').classList.add('hidden');
+}
+
+function toTraining() {
+    document.getElementById('login-container').classList.add('hidden');
+    document.getElementById('main-view').classList.add('hidden');
+    document.getElementById('tense-selection-container').classList.add('hidden');
+    document.getElementById('gap-text-container').classList.remove('hidden');
+
 }
 
 
 
 document.getElementById("login-button").addEventListener("click",toSuS);
 document.getElementById("home-button").addEventListener("click",toSuS);
+document.getElementById("start-training-button").addEventListener("click",toTraining);
 const destinations = document.getElementsByClassName("destination");
 
 // Loop through each element and add an event listener
